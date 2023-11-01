38e6b0ab4dc087471da3c1f794b55a77f225f0c1
d3169324 <d3169324@TU46944>
2023-11-01 10:53:41 +0000

    Finished worksheet extras

--- Maze/main.cpp
+++ Maze/main.cpp
@@ -10,150 +10,167 @@
 #include "Framework.h"
 using namespace std;
 
-// Screen dimensions
-int gScreenWidth{ 800 };
-int gScreenHeight{ 800 };
+int gScreenWidth{ 800 }, gScreenHeight{ 800 };  // Screen dimensions
 
-// Delay to slow things down
-int gTimeDelayMS{ 100 };
+int gTimeDelayMS{ 100 };  // Delay to slow things down
 
-// Maze size as constants
-constexpr int kMazeX{ 20 };
-constexpr int kMazeY{ 20 };
+constexpr int kMazeX{ 20 }, kMazeY{ 20 };  // Maze size as constants
 
-/*
-	2 Dimensional Arrays
-	You can think of these as being an array of arrays
-	
-	The maze has kMazeRowsY columns across (20 by default) represented by an array
-	We then have kMazeColumnsX (20) of these arrays, one for each row
-	
-	Columns and rows can be confusing so we tend to prefer working with x and y
-	x is the horizontal axis (columns) across and y is the vertical axis (rows) down.
-
-	Each single item is called a cell. 
-	
-	E.g. to output the value of the cell at 16 X (column 16) and 1 Y (row 1) we would write:
-	cout << map[1][16] << endl; // outputs G
-*/
+const int numEnemies{ 5 }, enemySpeed{ 25 };  // Number of enemies and movement chance
+const int cellWidth{ gScreenWidth / kMazeX }, cellHeight{ gScreenHeight / kMazeY };  // Cell size
+int radius = (cellWidth > cellHeight ? cellWidth : cellHeight) / 3;  // Circle radius
+int numGoals{ 1 };  // Number of goals (counter)
 
 char map[kMazeX][kMazeY] = {
-	// 20 columns (x axis) by 20 rows (y axis)
-	// X0   X1   X2   X3   X4   X5   X6   X7   X8   X9   X10  X11  X12  X13  X14  X15  X16  X17  X18  X19 
-	{ 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W' },	//Y0
-	{ 'W', '.', '.', '.', '.', 'W', '.', '.', '.', 'W', 'W', '.', '.', '.', '.', 'W', '.', '.', '.', 'W' },	//Y1
-	{ 'W', '.', 'W', 'W', 'W', 'W', 'W', 'W', '.', 'W', '.', '.', 'W', 'W', 'W', 'W', 'W', 'W', '.', 'W' },	//Y2
-	{ 'W', '.', 'W', '.', '.', '.', 'W', '.', '.', 'W', '.', 'W', 'W', '.', 'W', '.', '.', '.', '.', 'W' },	//Y3
-	{ 'W', '.', 'W', '.', 'W', '.', 'W', '.', 'W', 'W', '.', 'W', 'W', '.', 'W', 'W', 'W', '.', 'W', 'W' },	//Y4
-	{ 'W', '.', 'W', '.', 'W', '.', '.', '.', '.', '.', '.', '.', 'W', '.', 'W', '.', '.', '.', '.', 'W' },	//Y5
-	{ 'W', '.', 'W', '.', 'W', 'W', 'W', '.', '.', 'W', 'W', '.', 'W', '.', '.', '.', 'W', '.', '.', 'W' },	//Y6
-	{ 'W', '.', 'W', '.', 'W', '.', 'W', 'W', 'W', 'W', 'W', '.', 'W', '.', 'W', '.', 'W', 'W', 'W', 'W' },	//Y7
-	{ 'W', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'W', '.', '.', '.', 'W', '.', '.', '.', '.', 'W' },	//Y8
-	{ 'W', '.', 'W', '.', 'W', '.', 'W', 'W', '.', 'W', 'W', 'W', '.', 'W', 'W', 'W', 'W', '.', 'W', 'W' },	//Y9
-	{ 'W', '.', 'W', 'W', 'W', 'W', 'W', 'W', '.', 'W', 'W', 'W', '.', 'W', '.', 'W', 'W', '.', 'W', 'W' },	//Y10
-	{ 'W', '.', '.', '.', '.', 'W', '.', '.', '.', 'W', 'W', '.', '.', '.', '.', 'W', '.', '.', '.', 'W' },	//Y11
-	{ 'W', '.', 'W', 'W', 'W', 'W', 'W', 'W', '.', '.', '.', '.', 'W', '.', 'W', 'W', 'W', 'W', '.', 'W' },	//Y12
-	{ 'W', '.', 'W', '.', '.', '.', 'W', '.', '.', 'W', 'W', '.', 'W', '.', 'W', '.', '.', '.', '.', 'W' },	//Y13
-	{ 'W', '.', 'W', '.', 'W', '.', 'W', '.', 'W', 'W', 'W', '.', 'W', '.', 'W', '.', 'W', 'W', 'W', 'W' },	//Y14
-	{ 'W', '.', 'W', '.', 'W', '.', '.', '.', '.', '.', 'W', '.', 'W', '.', 'W', '.', '.', '.', 'W', 'W' },	//Y15
-	{ 'W', '.', 'W', '.', 'W', 'W', 'W', '.', '.', 'W', 'W', '.', 'W', '.', '.', '.', 'W', '.', '.', 'W' },	//Y16
-	{ 'W', '.', 'W', '.', 'W', '.', 'W', 'W', 'W', 'W', '.', '.', 'W', 'W', 'W', '.', 'W', 'W', '.', 'W' },	//Y17
-	{ 'W', 'P', '.', '.', '.', '.', '.', '.', '.', 'W', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'W' },	//Y18
-	{ 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W' },	//Y19
+	{ 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W' },
+	{ 'W', '.', '.', '.', '.', 'W', '.', '.', '.', 'W', 'W', '.', '.', '.', '.', 'W', '.', '.', '.', 'W' },
+	{ 'W', '.', 'W', 'W', 'W', 'W', 'W', 'W', '.', 'W', '.', '.', 'W', 'W', 'W', 'W', 'W', 'W', '.', 'W' },
+	{ 'W', '.', 'W', '.', '.', '.', 'W', '.', '.', 'W', '.', 'W', 'W', '.', 'W', '.', '.', '.', '.', 'W' },
+	{ 'W', '.', 'W', '.', 'W', '.', 'W', '.', 'W', 'W', '.', 'W', 'W', '.', 'W', 'W', 'W', '.', 'W', 'W' },
+	{ 'W', '.', 'W', '.', 'W', '.', '.', '.', '.', '.', '.', '.', 'W', '.', 'W', '.', '.', '.', '.', 'W' },
+	{ 'W', '.', 'W', '.', 'W', 'W', 'W', '.', '.', 'W', 'W', '.', 'W', '.', '.', '.', 'W', '.', '.', 'W' },
+	{ 'W', '.', 'W', '.', 'W', '.', 'W', 'W', 'W', 'W', 'W', '.', 'W', '.', 'W', '.', 'W', 'W', 'W', 'W' },
+	{ 'W', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'W', '.', '.', '.', 'W', '.', '.', '.', '.', 'W' },
+	{ 'W', '.', 'W', '.', 'W', '.', 'W', 'W', '.', 'W', 'W', 'W', '.', 'W', 'W', 'W', 'W', '.', 'W', 'W' },
+	{ 'W', '.', 'W', 'W', 'W', 'W', 'W', 'W', '.', 'W', 'W', 'W', '.', 'W', '.', 'W', 'W', '.', 'W', 'W' },
+	{ 'W', '.', '.', '.', '.', 'W', '.', '.', '.', 'W', 'W', '.', '.', '.', '.', 'W', '.', '.', '.', 'W' },
+	{ 'W', '.', 'W', 'W', 'W', 'W', 'W', 'W', '.', '.', '.', '.', 'W', '.', 'W', 'W', 'W', 'W', '.', 'W' },
+	{ 'W', '.', 'W', '.', '.', '.', 'W', '.', '.', 'W', 'W', '.', 'W', '.', 'W', '.', '.', '.', '.', 'W' },
+	{ 'W', '.', 'W', '.', 'W', '.', 'W', '.', 'W', 'W', 'W', '.', 'W', '.', 'W', '.', 'W', 'W', 'W', 'W' },
+	{ 'W', '.', 'W', '.', 'W', '.', '.', '.', '.', '.', 'W', '.', 'W', '.', 'W', '.', '.', '.', 'W', 'W' },
+	{ 'W', '.', 'W', '.', 'W', 'W', 'W', '.', '.', 'W', 'W', '.', 'W', '.', '.', '.', 'W', '.', '.', 'W' },
+	{ 'W', '.', 'W', '.', 'W', '.', 'W', 'W', 'W', 'W', '.', '.', 'W', 'W', 'W', '.', 'W', 'W', '.', 'W' },
+	{ 'W', '.', '.', '.', '.', '.', '.', '.', '.', 'W', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'W' },
+	{ 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W' },
 };
 
-const int cellWidth = gScreenWidth / kMazeX;
-const int cellHeight = gScreenHeight / kMazeY;
-int playerX = 0;
-int playerY = 0;
-int numGoals = 1;
+struct pos {
+	int x{ 0 };
+	int y{ 0 };
+};
 
-void GetPlayer() {  // Get player postition
-	for (int y = 0; y < kMazeY; y++) {
-		for (int x = 0; x < kMazeX; x++) {
-			if (map[y][x] == 'P') {
-				playerX = x;
-				playerY = y;
-				return;
-			}
+pos Player, Goal, Enemies[numEnemies];  // Player and Goal positions
+
+pos RandSpace() {  // Find random space
+	pos Rand;
+	while (true) {  // Search until space found
+		Rand.x = rand() % (kMazeX - 2) + 1;  // Efficient rand (doesnt check outer walls)
+		Rand.y = rand() % (kMazeY - 2) + 1;
+		if (map[Rand.y][Rand.x] == '.') {
+			return Rand;  // Return space pos
 		}
 	}
 }
 
 void NewGoal() {  // Randomise new goal
-	int randX;
-	int randY;
 	while (true) {
-		randY = rand() % kMazeY;
-		randX = rand() % kMazeX;
-		if (map[randY][randX] == '.') {
-			map[randY][randX] = 'G';
-			break;
-		}
+		pos Rand = RandSpace();
+		map[Rand.y][Rand.x] = 'G';
+		break;
 	}
-	cout << "Find the new goal!" << endl;
 }
 
-void CheckGoal(int posX, int posY) {  // Check if x, y is a goal
-	if (map[posY][posX] == 'G') {
-		cout << "You found goal number " << numGoals++ << endl;
-		NewGoal();
+void UpdateEnemies() {  // Enemy movement
+	for (int i = 0; i < numEnemies; i++) {
+		if (rand() % enemySpeed == 0) {  // Decide to move
+			int direction = rand() % 4;  // Decide direction
+			map[Enemies[i].y][Enemies[i].x] = '.';  // Remove enemy
+			switch (direction) {
+			case 0:  // Up
+				Enemies[i].y--;  // Move enemy
+				if (map[Enemies[i].y][Enemies[i].x] == 'W' || map[Enemies[i].y][Enemies[i].x] == 'G')  // If wall or goal
+					Enemies[i].y++;  // Move back
+				break;
+			case 1:  // Down
+				Enemies[i].y++;
+				if (map[Enemies[i].y][Enemies[i].x] == 'W' || map[Enemies[i].y][Enemies[i].x] == 'G')
+					Enemies[i].y--;
+				break;
+			case 2:  // Left
+				Enemies[i].x--;
+				if (map[Enemies[i].y][Enemies[i].x] == 'W' || map[Enemies[i].y][Enemies[i].x] == 'G')
+					Enemies[i].x++;
+				break;
+			case 3:  // Right
+				Enemies[i].x++;
+				if (map[Enemies[i].y][Enemies[i].x] == 'W' || map[Enemies[i].y][Enemies[i].x] == 'G')
+					Enemies[i].x--;
+				break;
+			}
+
+			if (map[Enemies[i].y][Enemies[i].x] == 'P') {  // Check if enemy hit player
+				cout << "An enemy got you! You lose. (Score: " << GetElapsedTime() << ")" << endl;
+				Player.x = kMazeX + 1;
+				Player.y = kMazeY + 1;
+			}
+			map[Enemies[i].y][Enemies[i].x] = 'E';
+		}
 	}
 }
 
 void MovePlayer(EKeyPressed key) {  // Player movement
-	if (key == EKeyPressed::eUp) {
-		if (map[playerY - 1][playerX] != 'W') {
-			map[playerY][playerX] = '.';
-			playerY--;
-			CheckGoal(playerX, playerY);
-			map[playerY][playerX] = 'P';
-		}
+	if (key == EKeyPressed::eNone || Player.x > kMazeX && Player.y > kMazeY) { // If none or dead do nothing
+		return;
 	}
-	else if (key == EKeyPressed::eDown) {
-		if (map[playerY + 1][playerX] != 'W') {
-			map[playerY][playerX] = '.';
-			playerY++;
-			CheckGoal(playerX, playerY);
-			map[playerY][playerX] = 'P';
-		}
+	map[Player.y][Player.x] = '.';  // Remove player
+
+	switch (key) {
+	case EKeyPressed::eUp:  // Key pressed
+		Player.y--;  // Move player
+		if (map[Player.y][Player.x] == 'W')  // If wall move back
+			Player.y++;
+		break;
+	case EKeyPressed::eDown:
+		Player.y++;
+		if (map[Player.y][Player.x] == 'W')
+			Player.y--;
+		break;
+	case EKeyPressed::eLeft:
+		Player.x--;
+		if (map[Player.y][Player.x] == 'W')
+			Player.x++;
+		break;
+	case EKeyPressed::eRight:
+		Player.x++;
+		if (map[Player.y][Player.x] == 'W')
+			Player.x--;
+		break;
 	}
-	else if (key == EKeyPressed::eLeft) {
-		if (map[playerY][playerX - 1] != 'W') {
-			map[playerY][playerX] = '.';
-			playerX--;
-			CheckGoal(playerX, playerY);
-			map[playerY][playerX] = 'P';
-		}
+
+	if (map[Player.y][Player.x] == 'G') {  // Check if player hit goal
+		cout << "You found goal number " << numGoals++ << " (Score: " << GetElapsedTime() << ")" << endl;
+		NewGoal();  // Generate new goal
+		StartClock(); // Restart timer for next score
 	}
-	else if (key == EKeyPressed::eRight) {
-		if (map[playerY][playerX + 1] != 'W') {
-			map[playerY][playerX] = '.';
-			playerX++;
-			CheckGoal(playerX, playerY);
-			map[playerY][playerX] = 'P';
-		}
+	else if (map[Player.y][Player.x] == 'E') {  // Check if player hit enemy
+		cout << "An enemy got you! You lose. (Score: " << GetElapsedTime() << ")" << endl;
+		Player.x = kMazeX + 1;
+		Player.y = kMazeY + 1;
 	}
+	map[Player.y][Player.x] = 'P';  // Move player
 }
 
 void DrawMaze() {  // Draw the maze
-	for (int y = 0; y < kMazeY; y++) {
+	for (int y = 0; y < kMazeY; y++) {  // Loop through entire maze
 		for (int x = 0; x < kMazeX; x++) {
 			switch (map[y][x]) {
-			case 'W':
+			case 'W':  // Wall
 				ChangeColour(100, 100, 100);  // Grey
 				DrawRectangle(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
 				break;
-			case 'G':
+			case 'G':  // Goal
 				ChangeColour(50, 255, 50);  // Green
 				DrawRectangle(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
 				break;
-			case 'P': {
-				int radius = (cellWidth > cellHeight ? cellWidth : cellHeight) / 3;
+			case 'P':  // Player
 				ChangeColour(255, 255, 50);  // Yellow
 				DrawCircle(x * cellWidth + (radius / 2), y * cellHeight + (radius / 2), radius);
-				break;}
-			default:
+				break;
+			case 'E':  // Enemy
+				ChangeColour(255, 50, 50);  // Red
+				DrawCircle(x * cellWidth + (radius / 2), y * cellHeight + (radius / 2), radius);
+				break;
+			default:  // Other
 				break;
 			}
 		}
@@ -163,11 +180,24 @@ void DrawMaze() {  // Draw the maze
 int main()
 {
 	srand(time(NULL));  // Set random seed
-	GetPlayer();
-	NewGoal();  // Generate goal position on start
+	
+	Player = RandSpace();  // Random player start
+	map[Player.y][Player.x] = 'P';
+	
+	Goal = RandSpace();  // Random goal start
+	map[Goal.y][Goal.x] = 'G';
+
+	for (int i = 0; i < numEnemies; i++) {
+		Enemies[i] = RandSpace();  // Random enemy start
+		map[Enemies[i].y][Enemies[i].x] = 'E';
+	}
+
+	StartClock();  // Start score timer
 	while (UpdateFramework())
 	{
+
 		MovePlayer(GetLastKeyPressed());
+		UpdateEnemies();
 		DrawMaze();
 	}
 
